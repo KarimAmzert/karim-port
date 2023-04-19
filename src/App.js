84fdbeea6ab7a3b27230b54b6308fff3300a4fc5
Karim <KarimAmzert@hotmail.com>
2023-04-19 18:59:11 -0400

    eslint fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import  Portfolio  from "./Portfolio";
-import Menu from "./Menu";
 
 function App() {
 
